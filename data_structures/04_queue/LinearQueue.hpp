e52b85e1f80d252e682291d5f9d9a7ab653c7bc0
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2026-07-02 20:05:16 +0400

    feat: add display, size functionality.

--- data_structures/04_queue/LinearQueue.hpp
+++ data_structures/04_queue/LinearQueue.hpp
@@ -5,22 +5,22 @@
 class LinearQueue
 {
     int front;
-    int back;
+    int rear;
     int data[MAX_SIZE];
 
 public:
-    LinearQueue() : front(-1), back(-1)
+    LinearQueue() : front(-1), rear(-1)
     {
     }
 
     bool isEmpty() const
     {
-        return front == -1 && back == -1;
+        return front == -1 && rear == -1;
     }
 
     bool isFull() const
     {
-        return back == MAX_SIZE - 1;
+        return rear == MAX_SIZE - 1;
     }
 
     int peek() const
@@ -38,12 +38,12 @@ public:
 
         if (isEmpty())
         {
-            front = back = 0;
-            data[back] = x;
+            front = rear = 0;
+            data[rear] = x;
             return;
         }
 
-        data[++back] = x;
+        data[++rear] = x;
     }
 
     void dequeue()
@@ -51,12 +51,36 @@ public:
         if (isEmpty())
             throw std::out_of_range("Underflow");
 
-        if (front == back)
+        if (front == rear)
         {
-            front = back = -1;
+            front = rear = -1;
             return;
         }
 
         ++front;
+    }
+
+    int size()
+    {
+        if (isEmpty())
+            return 0;
+
+        return rear - front + 1;
+    }
+
+    void display()
+    {
+        if (isEmpty())
+        {
+            std::cout << "Queue is empty\n";
+            return;
+        }
+
+        for (int i = front; i <= rear; ++i)
+        {
+            std::cout << data[i] << ' ';
+        }
+
+        std::cout << '\n';
     }
 };
